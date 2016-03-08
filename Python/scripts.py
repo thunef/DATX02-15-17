b375cec5263ad7fd2100d608687b41c1a5ed6a5c
thune <fredrik.thune.94@gmail.com>
2016-03-08 11:15:42 +0100

    Added more commands in python script

--- Python/scripts.py
+++ Python/scripts.py
@@ -12,7 +12,7 @@ variables = {}
 speed = 0
 
 # put the userdefined lists with the content of that
-# list in a container, each list keeps its name  
+# list in a container, each list keeps its name
 def getLists(json_lists):
     for k in range(len(json_lists)):
         name = json_lists[k]['listName']
@@ -22,6 +22,25 @@ def getVariables(json_variables):
     for k in range(len(json_variables)):
         name = json_variables[k]['name']
         variables[name] = json_variables[k]['value']
+
+def getValue(cmd):
+    if cmd[0] == 'readVariable':
+        return variables[cmd[1]]
+    if cmd[0] == 'randomFrom:to:':
+        return randint(cmd[1],cmd[2])
+    return 0
+
+
+def isTrue(statement):
+    a = getValue(statement[1])
+    b = getValue(statement[2])
+
+    if statement[0] == '=':
+        return a == b
+    elif statement[0] == '<':
+        return a < b
+    elif statement[0] == '>':
+        return a > b
 
 def getJson(name):
     zip_file = zipfile.ZipFile(name)
@@ -46,37 +65,72 @@ def runCommand(command):
             i = i + 1
 #        stop() # gopigo
         speed = 0
-    elif script_str == "stopScripts":
-        print "stopScripts"
-#        stop()  # gopigo
-        speed = 0
-    elif script_str == "doIf":
+
+    elif script_str == 'accelerate:':
         print script_str
+#        increase_speed()
+
+    elif script_str == 'retardate:':
+        print script_str
+#        decrease_speed()
+
+    elif script_str == 'maxspeed:':
+        print script_str
+#        set_speed(255)
+
+    elif script_str == 'nospeed:':
+        print script_str
+#        set_speed(0)
+
+    elif script_str == "stopScripts":
+        print script_str
+#        stop()  # gopigo
+
+    elif script_str == "doIf":
         if isTrue(command[1]) :
-                runScript(command[2])
-    elif command[0] == "doIfElse":
-        print "doIfElse"
+            runScript(command[2])
+
+    elif script_str == "doIfElse":
         if isTrue(command[1]):
             runScript(command[2])
         else:
             runScript(command[3])
+
     elif script_str == "doForever":
-        print "looping forever"
         loop_commands = command[2]
         while True:
             print script_str[1]
             runCommands(loop_commands)
+
     elif script_str == "doRepeat":
-        print "doRepeat"
+        loop_times = command[1]
         loop_commands = command[2]
-        for k in range(script_for):
+        for k in range(loop_times):
             runCommands(loop_commands)
+
     elif script_str == 'setVar:to:':
         variables[command[1]] = command[2]
         print command[1],"  =  ",variables[command[1]]
+
     elif script_str == 'changeVar:by:':
         variables[command[1]] += command[2]
         print command[1],"  =  ",variables[command[1]]
+
+    elif script_str == 'led:on:':
+        print script_str
+#        led_on()
+    elif script_str == 'led:off:':
+        print script_str
+#        led_off():
+    elif script_str == 'servo:on':
+        print script_str
+#        enable_servo()
+    elif script_str == 'servo:off':
+        print script_str
+#        disable_servo()
+    elif script_str == 'servo:pos':
+        print script_str
+        servo(command[1] % 180)
 
 def runCommands(commands):
     for index in range(len(commands)):
