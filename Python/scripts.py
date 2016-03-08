86a4b91c06b328593f9ef8a53b5d596621d84f22
HSteenari <henrik@steenari.se>
2016-03-08 10:00:17 +0100

    more commands to run

--- Python/scripts.py
+++ Python/scripts.py
@@ -3,18 +3,25 @@ import json
 import os
 import shutil
 import zipfile
-#from collections import deque
+#import gopigo
+from collections import defaultdict
 
+lists     = {}
+variables = {}
 
-expressions = [
-    'doRepeat',
-    'doForever',
-    'forward:'
-    'turnRight:'
-    'turnLeft:'
-]
+speed = 0
 
-code_to_run = []
+# put the userdefined lists with the content of that
+# list in a container, each list keeps its name  
+def getLists(json_lists):
+    for k in range(len(json_lists)):
+        name = json_lists[k]['listName']
+        lists[name] = json_lists[k]['contents']
+
+def getVariables(json_variables):
+    for k in range(len(json_variables)):
+        name = json_variables[k]['name']
+        variables[name] = json_variables[k]['value']
 
 def getJson(name):
     zip_file = zipfile.ZipFile(name)
@@ -25,35 +32,68 @@ def getJson(name):
            return json_data
 
 def runCommand(command):
-    script_str = str(command[0])
+    script_str = command[0]
     script_for = command[1]
-    print "run:", script_str, "for:", script_for
+#    print "run:", script_str, "for:", script_for
+    if script_str == "forward:":
+        print "framAt:",  script_for
+        if speed == 0:
+#            increase_speed() # gopigo
+            speed = read_motor_speed()
+        i = 0
+        for k in range(script_for):
+            i = i + 1
+#        stop() # gopigo
+        speed = 0
+    elif script_str == "stopScripts":
+        print "stopScripts"
+#        stop()  # gopigo
+        speed = 0
+    elif script_str == "doIf":
+        print script_str
+        if isTrue(command[1]) :
+                runScript(command[2])
+    elif command[0] == "doIfElse":
+        print "doIfElse"
+        if isTrue(command[1]):
+            runScript(command[2])
+        else:
+            runScript(command[3])
+    elif script_str == "doForever":
+        print "looping forever"
+        loop_commands = command[2]
+        while True:
+            print script_str[1]
+            runCommands(loop_commands)
+    elif script_str == "doRepeat":
+        print "doRepeat"
+        loop_commands = command[2]
+        for k in range(script_for):
+            runCommands(loop_commands)
+    elif script_str == 'setVar:to:':
+        variables[command[1]] = command[2]
+        print command[1],"  =  ",variables[command[1]]
+    elif script_str == 'changeVar:by:':
+        variables[command[1]] += command[2]
+        print command[1],"  =  ",variables[command[1]]
 
 def runCommands(commands):
     for index in range(len(commands)):
         runCommand(commands[index])
 
-def getCommands(json_data):
-    command_list = []
-    for k in range(len(json_data[0][2])):
-#        print json_data[0][2][k]
-        tmp = json_data[0][2][k]
-        print "tmp:", tmp
-        for n in range(len(tmp)):
-            if tmp[n] == 'doRepeat':
-
-
-    print "Command_list:", command_list
-    return command_list
 
 def start(name):
     json_raw_data = getJson(name)
     json_data     = json.loads(json_raw_data)
+#    json_scripts  = []
     json_scripts  = json_data['children'][0]['scripts']
+    json_lists    = json_data['lists']
 
-    command_list = getCommands(json_scripts)
+    getVariables(json_data['variables'])
+    getLists(json_lists)
+    runCommands(json_scripts)
 
-    runCommands(command_list)
+
 
 if __name__ == "__main__":
     import sys
